a5fc5136d66a46247a815e77d6102453acd1a79d
Miralhas <victor2.miralhas@gmail.com>
2025-10-01 22:54:34 -0300

    feat: main cli

---
+++ stalkers-cli/__main__.py
@@ -0,0 +1,15 @@
+import novel
+import req
+import typer
+
+app = typer.Typer(
+    help="Collection of scripts that clean, extract, make requests and format a novel.",
+    add_completion=False,
+    no_args_is_help=True,
+    
+)
+app.add_typer(novel.app, name="novel", help="Extract metadata and format chapters of a novel")
+app.add_typer(req.app, name="req", help="Send requests to the Stalkers API")
+
+if __name__ == "__main__":
+    app()
